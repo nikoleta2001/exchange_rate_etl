69749fcb6ffe460ff43f7f1cd403cb1001f7f036
nikoleta.b.001@gmail.com <nikoleta.b.001@gmail.com>
2025-10-15 22:15:09 +0200

    Structure fixes

--- etl/main.py
+++ etl/main.py
@@ -1,9 +1,16 @@
 from extract import fetch_ecb_csv
 from transform import melt_ecb
 from load import upsert_fx_rates
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 if __name__ == "__main__":
-    df_raw = fetch_ecb_csv()
-    df_tidy = melt_ecb(df_raw)
-    upsert_fx_rates(df_tidy)
-    print("ETL done")
+    try:
+        df_raw = fetch_ecb_csv()
+        df_tidy = melt_ecb(df_raw)
+        upsert_fx_rates(df_tidy)
+        print("ETL done")
+    except Exception as e:
+        logging.exception("ETL failed: %s", e)
+        raise
